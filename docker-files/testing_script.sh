2ac669e878204038dfe4b6ee6bd42350afc9e5d8
kushalsingh007 <kushal.spiderman.singh@gmail.com>
2015-11-22 02:14:50 +0530

    Adding a testing script for executing commands in docker - Can be modified to copy and delete stuff from docker container - Can be remove and run container

---
+++ docker-files/testing_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run --name ${JOB_NAME} -v ${WORKSPACE}:/project zapier/project-tests
+
+# copy build artifacts out of the container
+docker cp ${JOB_NAME}:/project/nosetests.xml .
+docker cp ${JOB_NAME}:/project/coverage.xml .
+docker cp ${JOB_NAME}:/project/cover .
+
+# remove the container
+docker rm ${JOB_NAME}
