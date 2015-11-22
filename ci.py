f7ffaf1bd0a677e7658d12308d6f36b4d0201465
AadityaNair <aadityanair6494@gmail.com>
2015-11-22 15:05:05 +0530

    added command line arguments. Still a stub.

--- ci.py
+++ ci.py
@@ -6,3 +6,30 @@
     Shows status of last job or all jobs.
 """
 
+def show_last_status():
+    pass
+
+def show_all_status():
+    pass
+
+
+# --------------------------------------------------------------
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument('status',
+        type=str,
+        help="takes either 'last' or 'all'. Prints either status of last job or all jobs. "
+        )
+
+args = parser.parse_args()
+status = args.status.lower()
+
+if status == 'last':
+    show_last_status()
+elif status == 'all':
+    show_all_status()
+else:
+    print "Wrong argument. Try again.\n"
+
+
