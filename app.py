41dcd236c688a1e2834978b2f24755eef56b1c33
AadityaNair <aadityanair6494@gmail.com>
2015-12-08 13:57:25 +0530

    Removed dummy data

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask, render_template, request, json, Response
-from flask.ext.pymongo import PyMongo
-
+from flask import Flask, request, json, Response
 app = Flask(__name__)
-mongo = PyMongo(app)
 
 @app.route("/")
 def main():
@@ -10,16 +7,14 @@ def main():
 
 @app.route("/ci/build", methods=['GET', 'POST'])
 def build():
-    print "XYZ"
     content = request.json
     print content
-    data = {
-        'hello'  : 'world',
-        'number' : 3
-    }
-    js = json.dumps(data)
-    resp = Response(js, status=200, mimetype='application/json')
-    return resp 
+    
+    git_dir = str(content[u'git_dir'])
+    sha1 = str(content[u'id'])
+    msg = str(content[u'msg'])
+    
+    return Response(request.json, status=200, mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(threaded=True, debug=True)
