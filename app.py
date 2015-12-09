5cde9f8702a2af8c724429492d5f2a6670f84601
AadityaNair <aadityanair6494@gmail.com>
2015-12-09 13:49:09 +0530

    Added previous build status lookup

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, json, Response
+from flask import Flask, request, Response, jsonify
 app = Flask(__name__)
+
+import db
 
 @app.route("/")
 def main():
-    return "Nothing here"
+    return "Nothing here", 408
 
 @app.route("/ci/build", methods=['GET', 'POST'])
 def build():
@@ -13,8 +15,18 @@ def build():
     git_dir = str(content[u'git_dir'])
     sha1 = str(content[u'id'])
     msg = str(content[u'msg'])
-    
+    db.new_commit(git_dir, sha1, msg)
+
     return Response(request.json, status=200, mimetype='application/json')
+
+@app.route("/ci/status/all")
+def status_all():
+    return jsonify(db.find_build(allbuilds=True))
+
+@app.route("/ci/status/last")
+def status_last():
+    return jsonify(db.find_build(allbuilds=False))
+
 
 if __name__ == "__main__":
     app.run(threaded=True, debug=True)
--- ci.py
+++ ci.py
@@ -5,13 +5,14 @@
     Main File. Accepts user input.
     Shows status of last job or all jobs.
 """
-
+import requests
 def show_last_status():
-    pass
+    resp = requests.get("http://127.0.0.1:5000/ci/status/last")
+    return resp.json()
 
 def show_all_status():
-    pass
-
+    resp = requests.get("http://127.0.0.1:5000/ci/status/all")
+    return resp.json()
 
 # --------------------------------------------------------------
 import argparse
